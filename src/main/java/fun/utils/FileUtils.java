3775940fcb735ddd6a6e998c42f8cffa8dc20d89
Elias T <20283484+elias2t@users.noreply.github.com>
2022-12-08 11:10:34 +0100

    Add util to read file entirly not only line by line

--- src/main/java/fun/utils/FileUtils.java
+++ src/main/java/fun/utils/FileUtils.java
@@ -2,6 +2,10 @@ package fun.utils;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.core.io.ClassPathResource;
 
@@ -9,6 +13,15 @@ public class FileUtils {
 
   public static FileReader fromResources( String path ) throws IOException {
     return new FileReader( new ClassPathResource( path ).getFile() );
+  }
+
+  public static Stream<String> fromResourcesAsStream( String path ) throws IOException {
+    return Files.lines( new ClassPathResource( path ).getFile().toPath() );
+  }
+
+  public static List<String> fromResourcesAsList( String path ) throws IOException {
+    return Files.lines( new ClassPathResource( path ).getFile().toPath() )
+        .collect( Collectors.toList() );
   }
 
 }

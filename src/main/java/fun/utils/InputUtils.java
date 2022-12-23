c016f3c0d7aad7631a6a1d857c797ee8cd980f67
Elias T <20283484+elias2t@users.noreply.github.com>
2022-12-23 10:27:49 +0100

    Add Point3D object to InputUtils

--- src/main/java/fun/utils/InputUtils.java
+++ src/main/java/fun/utils/InputUtils.java
@@ -2,11 +2,34 @@ package fun.utils;
 
 import java.awt.*;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 public class InputUtils {
 
   public static Point sToPoint( String point ) {
     String[] coordinates = point.split( "," );
     return new Point( Integer.parseInt( coordinates[0] ), Integer.parseInt( coordinates[1] ) );
+  }
+
+  public static Point3D sTo3DPoint( String point ) {
+    String[] coordinates = point.split( "," );
+    return new Point3D(
+        Integer.parseInt( coordinates[0] ),
+        Integer.parseInt( coordinates[1] ),
+        Integer.parseInt( coordinates[2] ) );
+  }
+
+  @Data
+  @AllArgsConstructor
+  public static class Point3D {
+
+    public int x;
+
+    public int y;
+
+    public int z;
+
   }
 
 }
